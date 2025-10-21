5301769cd3b8d58594d12e666cfb4b18bb6f7160
Sandra Allies <sandra.allies.fr@gmail.com>
2025-10-21 11:31:39 +0200

    Formulaire avec champ texte et bouton ajouter, champ vide ou avec espace refusé, champ qui se vide automatiquement après le submit, nouvelle tâche qui apparait instantanément et sans rechargement de page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useState } from "react";
 import CheckBox from "./components/CheckBox.jsx";
 import Formulaire from "./components/Formulaire.jsx";
 import Titre from "./components/Titre.jsx";
 
 function App() {
+      const [tasks, setTasks] = useState([
+        { id: 1, name: "Faire les courses", checked: false },
+        { id: 2, name: "Apprendre React", checked: false },
+        { id: 3, name: "Préparer la présentation", checked: false },
+        { id: 4, name: "Réviser Javascript", checked: false },
+      ]);
+      
+  const handleClick = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      )
+    );
+  };
+
+      const ajouterTache = (nomTache) => {
+        if (!nomTache.trim()) return;
+
+        const nouvelleTache = {
+          id: Date.now(),
+          name: nomTache,
+          checked: false,
+        };
+
+        setTasks([...tasks, nouvelleTache]);
+      }
+
   return (
     <div
       className="p-6 lg:m-6 w-screen lg:w-4/5 h-full
@@ -11,8 +39,8 @@ function App() {
       lg:border-4 lg:rounded-lg lg:border-gray-300 font-sans"
     >
       <Titre />
-      <Formulaire />
-      <CheckBox />
+      <Formulaire onAddTask={ajouterTache} />
+      <CheckBox afficherTache={tasks} handleClick={handleClick} />
     </div>
   );
 }
